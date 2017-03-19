27ff4954ba8697d4598181efcc4b2ea4f165018b
Grace Liang <acie.liang@gmail.com>
2017-03-19 12:45:11 -0700

    final go backend

--- alexa.go
+++ alexa.go
@@ -18,7 +18,8 @@ import (
 )
 
 var (
-	TriviaList []QuizletQuestion
+	MasterQuestionList []QuizletQuestion
+	DB *sql.DB
 )
 
 type QuizletQuestion struct {
@@ -34,17 +35,47 @@ type QuizletReply struct {
 type QuizletQuestions []QuizletQuestion
 
 func main() {
-	InitializeQuestions()
-	InitializeDatabase()
-	fmt.Println("Question list:", TriviaList)
+	/*
+	Database name: questions
+	Admin user: maxroach 
+	*/
+	db, err := sql.Open("postgres", "postgresql://maxroach@localhost:26257/questions?sslmode=disable")
+	if err != nil {
+		log.Fatalf("error connection to the database: %s", err)
+	}
+	DB = db
+	defer db.Close()
+
+	InitializeQuestions(db)
 
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", Index)
+    router.HandleFunc("/categories", GetCategories)
     router.HandleFunc("/random", RandomizeQuestions)
     log.Fatal(http.ListenAndServe(":8081", router))
 }
 
-func InitializeQuestions() {
+func InitializeQuestions(db *sql.DB) {
+	GetTriviaQuestions(db)
+	//GetSpaceQuestions(db)
+}
+
+func GetTriviaQuestions(db *sql.DB) {
+	/*
+	rows, err := db.Query("SELECT Question FROM Questions WHERE Type = 'Trivia'")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		// there are rows, don't bother requesting the questions again
+		return
+	}
+	*/
+
+	fmt.Println("Reparsing Trivia")
+	// Database empty: parse questions
 	s := "https://api.quizlet.com/2.0/sets/118552361?client_id=ypc6Ext7PE&whitespace=1"
 	resp, err := http.Get(s)
 	if err != nil {
@@ -66,26 +97,7 @@ func InitializeQuestions() {
 			fmt.Println("Can't unmarshal", err)
 			os.Exit(1)
 		}
-
-		//fmt.Println(m)
 	}
-
-	for _, q := range m.Questions {
-		TriviaList = append(TriviaList, q)
-	}
-}
-
-/*
-Database name: questions
-Admin user: maxroach 
-*/
-
-func InitializeDatabase() {
-	db, err := sql.Open("postgres", "postgresql://maxroach@localhost:26257/questions?sslmode=disable")
-	if err != nil {
-		log.Fatalf("error connection to the database: %s", err)
-	}
-	defer db.Close()
 
 	// Drop previously existing "questions" table.
 	if _, err := db.Exec(
@@ -99,17 +111,26 @@ func InitializeDatabase() {
 		log.Fatal(err)
 	}
 
+	fmt.Println("Questions len", len(m.Questions))
 	// Insert all of the questions and all of the answers
-	for _, q := range TriviaList {
-		if (strings.Index(q.Question, "'") == -1) && (strings.Index(q.Answer, "'") == -1) {
+	for _, q := range m.Questions {
+		// Take questions and answers without aposterphes
+		// and questions with answers 2 words or less
+		// and questions with more than 2 words
+		if (strings.Index(q.Question, "'") == -1) && (strings.Index(q.Answer, "'") == -1) && 
+		(len(strings.Fields(q.Answer)) < 3) && (len(strings.Fields(q.Question)) > 2) {
 			s := fmt.Sprintf("INSERT INTO Questions (Question, Answer, Type) VALUES ('%s', '%s', 'Trivia')", 
 				q.Question, q.Answer)
+			MasterQuestionList = append(MasterQuestionList, q)
 			if _, err := db.Exec(s); err != nil {
 				log.Fatal(err)
 			}
 		}
 	}
+}
 
+func GetSpaceQuestions(db *sql.DB) {
+	/*
 	rows, err := db.Query("SELECT Question FROM Questions")
 	if err != nil {
 		log.Fatal(err)
@@ -117,20 +138,93 @@ func InitializeDatabase() {
 	defer rows.Close()
 
 	for rows.Next() {
-		var question string
-		if err := rows.Scan(&question); err != nil {
-			log.Fatal(err)
+		// there are rows, don't bother requesting the questions again
+		return
+	}
+	*/
+
+	// Database empty: parse Space
+	s := "https://api.quizlet.com/2.0/sets/119475430?client_id=ypc6Ext7PE&whitespace=1"
+	resp, err := http.Get(s)
+	if err != nil {
+		fmt.Println("Malformed get", err)
+		os.Exit(1)
+	}
+	bytes, _ := ioutil.ReadAll(resp.Body)
+
+
+	// Find index of terms
+	idx := strings.Index(string(bytes), "\"terms\":")
+	m := new(QuizletReply)
+	if idx > 0 {
+		substring := string(bytes)[idx:len(string(bytes))]
+		substring = "{" + substring
+		//fmt.Println(substring)
+
+		err = json.Unmarshal([]byte(substring), &m)
+		if err != nil {
+			fmt.Println("Can't unmarshal", err)
+			os.Exit(1)
 		}
-		fmt.Println("Question:", question)
+	}
+
+	
+	// Drop previously existing "space" table.
+	if _, err := db.Exec(
+		"DROP TABLE Space"); err != nil {
+		//log.Fatal(err)
+	}
+
+	// Create the "questions" table.
+	if _, err := db.Exec(
+		"CREATE TABLE IF NOT EXISTS Space (ID SERIAL PRIMARY KEY, Question varchar(1024), Answer varchar(1024), Type varchar(32))"); err != nil {
+		log.Fatal(err)
+	}
+
+	// Insert all of the questions and all of the answers
+	fmt.Println("Space Questions len", len(m.Questions))
+	for _, q := range m.Questions {
+		// Take questions and answers without aposterphes
+		if (strings.Index(q.Question, "'") == -1) && (strings.Index(q.Answer, "'") == -1) {
+			s := fmt.Sprintf("INSERT INTO Space (Question, Answer, Type) VALUES ('%s', '%s', 'Space')", 
+				q.Answer, q.Question)
+			MasterQuestionList = append(MasterQuestionList, q)
+			if _, err := db.Exec(s); err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
+
 
 func Index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello world, %q", html.EscapeString(r.URL.Path))
 }
 
-func RandomizeQuestions(w http.ResponseWriter, r *http.Request) {
-	rand.Seed(time.Now().Unix())
+func GetCategories(w http.ResponseWriter, r *http.Request) {
+	rows, err := DB.Query("SELECT DISTINCT q.Type, s.Type FROM questions AS q, Space AS s")
+	if err != nil {
+		log.Fatal("Error parsing categories")
+	}
+	defer rows.Close()
 
-	json.NewEncoder(w).Encode(TriviaList[rand.Intn(len(TriviaList))])
+	categories := make([]string, 0)
+	for rows.Next() {
+		// TODO: this is hacky -- every time you add a table, this needs a new entry
+		var trivia string
+		var space string
+		err = rows.Scan(&trivia, &space)
+
+		categories = append(categories, trivia)
+		categories = append(categories, space)
+	}
+
+	json.NewEncoder(w).Encode(categories)
+}
+
+func RandomizeQuestions(w http.ResponseWriter, r *http.Request) {
+	//rows, err := DB.Query("SELECT * FROM Questions, Space")
+
+	rand.Seed(time.Now().Unix())
+	json.NewEncoder(w).Encode(MasterQuestionList[rand.Intn(len(MasterQuestionList))])
 }
